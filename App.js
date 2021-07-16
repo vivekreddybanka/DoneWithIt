a05f112f34e46af094a23d4ff3010faa1b58016c
vivekreddybanka <banka.vivek94@gmail.com>
2021-07-15 22:08:14 -0500

    gitignore is change to ignore expo files

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1}>Open up App.js to start working on your app! make this very long;</Text>
+      <Image source={require("./assets/icon.png")}></Image>
+    </SafeAreaView>
   );
 }
 
