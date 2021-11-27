9dcf65d5fa228cc630b7e9bd3ef803e326c98fcc
vivekreddybanka <banka.vivek94@gmail.com>
2021-11-27 11:43:45 -0600

    added the netinfo for networkstorage.

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import {  Button, StyleSheet, Image, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import navigationTheme from './navigation/navigationTheme';
 import AppNavigator from './navigation/AppNavigator';
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+  // const netinfo = useNetInfo();
+
+  // console.log(netinfo)
   return (
     <NavigationContainer theme={navigationTheme}>
         {/* <AuthNavigator></AuthNavigator> */}
