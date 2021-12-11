c7c9f86370edaacb8166d09bc205de048b75aaf4
vivekreddybanka <banka.vivek94@gmail.com>
2021-12-11 13:35:07 -0600

    added the push notification.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,15 +2,33 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import ListingScreen from "../screens/ListingScreen";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigation from "./AccountNavigation";
 import TabBarIcon from "../Componets/TabBarIcons";
 import NewListingButton from "./NewListingButton";
 import route from "./route";
-
+import * as Notifications  from 'expo-notifications'
+import * as permissions from 'expo-permissions'
 const Tabs = new createBottomTabNavigator();
 const AppNavigator = () => {
+    useEffect(() => {
+        registerForPushNotifications();
+    },[])
+    const registerForPushNotifications = async () => {
+        try {
+            const Permission = await permissions.askAsync(permissions.NOTIFICATIONS);
+            if(!Permission.granted) {
+                return;
+            }
+            const token = await Notifications.getExpoPushTokenAsync();
+            console.log(token)
+        } catch (error) {
+            console.log('Error in the push notification', error)
+        }
+       
+    }
+
    return ( <Tabs.Navigator screenOptions={{ showIcon: true }} >
         <Tabs.Screen name = "Feed" component={FeedNavigator} 
         options={{
